92af9d51e09e6164e86f4f71f303cded1eacaf07
NoriakiHoriuchi <youpsit@gmail.com>
2014-09-14 14:03:33 +0900

    Finish chap7; finish user signup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Railssample::Application.initialize!
+
+Railssample::Application.configure do
+	# bcrypt'のコスト関数を下げることでテストの速度を向上させる。
+	ActiveModel::SecurePassword.min_cost = true
+end
